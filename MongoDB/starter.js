dbe497fe018cc955b191dd1b8bbb36476932f62b
utkarsh <utkarshgupta.281@gmail.com>
2026-02-17 16:57:21 +0530

    add mongodb compass intro

--- MongoDB/starter.js
+++ MongoDB/starter.js
@@ -6,7 +6,8 @@
 //? information --> processed data : ()
 //~ ashwin has scored 34 in html exam
 
-//TODO: data or information what is stored in DB?
+//? or information what is stored in DB?
+//! data
 
 //? database --> collection of information, container used to store data, fetch, manage
 
@@ -71,3 +72,96 @@ let e5 = "another value";
 //     col2: id -> 123,
 //     col3: age -> 23
 //     col4:email -> ""
+
+//! difference between sql and no-sql dbs
+
+//? sql (RDBMS)
+//! schema is fixed or static
+//! data is stored in tables (rows and cols)
+//! it supports vertical scaling
+//! suitable for cases where data integrity and reference is important
+//! supports ACID transaction
+//! it supports data rollback (deleted << undo)
+//! examples -> mysql, postgres, ect..
+
+//? no-sql
+//! schema is dynamic or flexible
+//! data is stored in documents, key-value, graphs, wide columns
+//! it supports horizontal scaling
+//! suitable for cases where data is unstructured or semi-structured
+//! it supports BASE transaction (and some concepts of ACID)
+//! it does not support data rollback
+//! examples -> mongodb, redis, hBase..
+
+//? data integrity -> emp table (name, id, deptNo); dept table (id, dName, location)
+//~ "abc", 123, 20
+//~ "abc", 123, 80
+//! 10 ,20 ,30 deptNos
+
+//! ++++++++++++++ scaling ++++++++++++++++++++++++++
+//? --> scaling refers to the altering the capacity or size (RAM, storage) of the system
+
+let myLaptop = {
+  ram: 8,
+  storage: 512,
+};
+
+//? availableResource = 1 gb ram left and 1 gb storage left
+
+//! 1) DELETE THE EXISTING RESOURCES X
+
+//! 2) UPGRADE THE EXISTING SYSTEM : vertical scaling
+//? in this new resources are added to the existing system
+let myLaptopUpgraded = {
+  ram: 16,
+  storage: 1024,
+};
+
+//! 3) BUY A NEW SYSTEM : horizontal scaling
+//? in this new resources are added with the existing system
+let myNewLaptop = {
+  ram: 8,
+  storage: 512,
+};
+
+//? distributed and highly available (db)
+
+//! acid -> atomicity (all or none, no checkpoints), consistency, isolation, durability (persistence)
+//? back application
+//! bank app
+
+//! base -> basically available, soft state, eventually consistent
+//? any social media app instagram
+//~ soft state : data will change without any user interference
+
+//!
+//? CAP theorem -> consistency, availability, partial tolerance(must)
+//? pa: social media
+//? pc: bank app
+
+//! sql and nosql --> data is structured, relation : sql
+//? unstructured, large amount -> nosql
+
+//? netflix --> cassendraDb, sql
+//? polyglot applications: node, java, python
+// ? polyglot persistence
+
+//! =========================== MONGODB --> ======================================
+//~ it is a document based no-sql database which stores data in JSON like format (BSON)
+
+//! sql
+//? database (name) >> table (name) >> cols (input value) >> rows
+
+//! mongodb
+//? database (name) >> collections (name) >> documents{} >> key-value pair
+
+//? mongodb server
+
+//? https://www.google.com
+
+//? mongodb://localhost:27017
+//~ "mongodb": protocol
+//~ "localhost": domain name
+//~ "27017": port number
+
+//! mongodb compass is a GUI (graphical user interface) through which we can interact with database without writing any piece of code (we can interact visually)
